384490f49153a4846b4dc9d6822dc31dbfcc68a0
kollinslima <kollins.lima@gmail.com>
2017-11-17 17:27:52 +0000

    Bug fix - subida na recursao

--- Btree/insert_btree.c
+++ Btree/insert_btree.c
@@ -123,19 +123,21 @@ int insert_btree(FILE *fi, int RRN, tKey key, tKey *propo_key, int *propo_r_chil
                     
                     split(fi, &actual_page, RRN, aux_key, aux_r_child, propo_key, propo_r_child);
                     
-                    actual_page = new_page();            //"Sobe na recursão"
-                    actual_page.count += 1;
-                    actual_page.keys[actual_page.count-1] = (*propo_key);
-                    actual_page.children[actual_page.count-1] = RRN;
-                    actual_page.children[actual_page.count] = (*propo_r_child);
-                    actual_page.isLeaf = 0;
-        
-                    write_page(fi, actual_page, get_free_RRN(fi));
-                    assert(printf("Escrita Pag: %d\n", get_free_RRN(fi)));
-        
-                    update_root(fi, get_free_RRN(fi));
-                    update_free_slot(fi, get_free_RRN(fi) + 1);
-                    
+                    //Não existe nó acima
+                    if(get_root_RRN(fi) == RRN){
+                        actual_page = new_page();            //"Sobe na recursão"
+                        actual_page.count += 1;
+                        actual_page.keys[actual_page.count-1] = (*propo_key);
+                        actual_page.children[actual_page.count-1] = RRN;
+                        actual_page.children[actual_page.count] = (*propo_r_child);
+                        actual_page.isLeaf = 0;
+            
+                        write_page(fi, actual_page, get_free_RRN(fi));
+                        assert(printf("Escrita Pag: %d\n", get_free_RRN(fi)));
+            
+                        update_root(fi, get_free_RRN(fi));
+                        update_free_slot(fi, get_free_RRN(fi) + 1);
+                    }
                 }
             }
         }
